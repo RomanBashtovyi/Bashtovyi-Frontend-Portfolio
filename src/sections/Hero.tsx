cd3c8930bb36a1aed29159c6cfc38b74f5caab99
Roman Bashtovyi <roman.bashtovyy@gmail.com>
2025-09-25 15:40:52 +0300

    feat(hero): implement Hero section; add HeroOrbit component

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,117 @@
+import memojiImage from '@/assets/images/avatar.png'
+import Image from 'next/image'
+import ArrowDown from '@/assets/icons/arrow-down.svg'
+import grainImage from '@/assets/images/grain.jpg'
+import { HeroOrbit } from '@/components/HeroOrbit'
+import StarIcon from '@/assets/icons/star.svg'
+import SparkleIcon from '@/assets/icons/sparkle.svg'
+
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
-};
+  return (
+    <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
+      <div
+        className="absolute inset-0 "
+        style={{
+          maskImage:
+            'linear-gradient(to bottom, transparent, black 10%, black 70%, transparent)',
+          WebkitMaskImage:
+            'linear-gradient(to bottom, transparent, black 10%, black 70%, transparent)',
+        }}
+      >
+        <div
+          className="absolute inset-0 -z-30 opacity-5"
+          style={{
+            backgroundImage: `url(${grainImage.src})`,
+          }}
+        ></div>
+
+        <div className="size-[820px] hero-ring"></div>
+        <div className="size-[1020px] hero-ring"></div>
+        <div className="size-[1220px] hero-ring"></div>
+        <HeroOrbit size={800} rotation={-75}>
+          <StarIcon className="size-28 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={550} rotation={20}>
+          <StarIcon className="size-12 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={590} rotation={98}>
+          <StarIcon className="size-8 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={290} rotation={98}>
+          <StarIcon className="size-8 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={330} rotation={10}>
+          <StarIcon className="size-8 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={430} rotation={-15}>
+          <SparkleIcon className="size-8 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={440} rotation={80}>
+          <SparkleIcon className="size-5 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={530} rotation={180}>
+          <SparkleIcon className="size-10 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={710} rotation={145}>
+          <SparkleIcon className="size-14 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={250} rotation={145}>
+          <SparkleIcon className="size-14 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={720} rotation={85}>
+          <div className="size-3 bg-emerald-300/20 rounded-full" />
+        </HeroOrbit>
+        <HeroOrbit size={520} rotation={-40}>
+          <div className="size-2 bg-emerald-300/20 rounded-full" />
+        </HeroOrbit>
+        <HeroOrbit size={650} rotation={-5}>
+          <div className="size-3 bg-emerald-300/20 rounded-full" />
+        </HeroOrbit>
+        <HeroOrbit size={150} rotation={-5}>
+          <div className="size-5 bg-emerald-300/20 rounded-full" />
+        </HeroOrbit>
+      </div>
+      <div className="container">
+        <div className="flex flex-col items-center">
+          <Image
+            src={memojiImage}
+            className="size-[100px]"
+            alt="Person peeking from behind computer"
+          />
+          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
+            <div className="bg-green-500 size-2.5 rounded-full"></div>
+            <div className="text-sm font-medium">
+              Available for new projects
+            </div>
+          </div>
+        </div>
+        <div className="max-w-lg mx-auto">
+          <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
+            Hello, I&apos;m{' '}
+            <span className="text-white">
+              Roman Bashtovyi
+            </span>
+          </h1>
+          <p className="mt-4 text-center text-white/60 md:text-lg">
+            I&apos;m a frontend developer with a passion for
+            creating beautiful and functional websites.
+          </p>
+        </div>
+        <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
+          <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+            <span className="font-semibold">
+              Explore My Work
+            </span>
+            <ArrowDown className="size-4" />
+          </button>
+          <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 px-6 h-12 rounded-xl">
+            <span>👋</span>
+            <span className="font-semibold">
+              Let&apos;s Connect
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
