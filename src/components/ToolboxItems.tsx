e4e4222641076d66fcec2c35c5f134b0adfd9e8f
Roman Bashtovyi <roman.bashtovyy@gmail.com>
2025-10-06 12:41:40 +0300

    feat(about): add 'move-right' animation and enhance ToolboxItems with new layout and animation effects

--- src/components/ToolboxItems.tsx
+++ src/components/ToolboxItems.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react'
 import { TechIcon } from './TechIcon'
 import { twMerge } from 'tailwind-merge'
 
@@ -26,16 +27,20 @@ export const ToolboxItems = ({
           itemsWrapperClassName
         )}
       >
-        {items.map((item) => (
-          <div
-            key={item.title}
-            className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg"
-          >
-            <TechIcon component={item.iconType} />
-            <span className="text-semibold">
-              {item.title}
-            </span>
-          </div>
+        {[...new Array(2)].fill(0).map((_, idx) => (
+          <Fragment key={idx}>
+            {items.map((item) => (
+              <div
+                key={item.title}
+                className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg"
+              >
+                <TechIcon component={item.iconType} />
+                <span className="font-semibold">
+                  {item.title}
+                </span>
+              </div>
+            ))}
+          </Fragment>
         ))}
       </div>
     </div>
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { SectionHeader } from '@/components/SectionHeader'
 import { Card } from '@/components/Card'
 import StarIcon from '@/assets/icons/star.svg'
@@ -14,6 +15,8 @@ import mapImage from '@/assets/images/map.png'
 import smileEmoji from '@/assets/images/memoji-smile.png'
 import { CardHeader } from '@/components/CardHeader'
 import { ToolboxItems } from '@/components/ToolboxItems'
+import { motion } from 'framer-motion'
+import { useRef } from 'react'
 
 const toolboxItems = [
   {
@@ -88,6 +91,7 @@ const hobbies = [
 ]
 
 export const AboutSection = () => {
+  const constraintRef = useRef(null)
   return (
     <div className="py-20 lg:py-28">
       <div className="container">
@@ -112,11 +116,14 @@ export const AboutSection = () => {
                 title="My Toolbox"
                 description="Explore my technologies and tools I use to craft exceptional digital experiences."
               />
-              <ToolboxItems items={toolboxItems} />
+              <ToolboxItems
+                items={toolboxItems}
+                itemsWrapperClassName="animate-move-left [animation-duration:30s]"
+              />
               <ToolboxItems
                 items={toolboxItems}
                 className="mt-6 "
-                itemsWrapperClassName="-translate-x-1/2"
+                itemsWrapperClassName=" animate-move-right [animation-duration:20s]"
               />
             </Card>
           </div>
@@ -127,21 +134,26 @@ export const AboutSection = () => {
                 description="Explore my interests and hobbies beyond the digital realm."
                 className="px-6 py-6"
               />
-              <div className="relative flex-1">
+              <div
+                className="relative flex-1"
+                ref={constraintRef}
+              >
                 {hobbies.map((hobby) => (
-                  <div
+                  <motion.div
                     key={hobby.title}
                     className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute"
                     style={{
                       left: hobby.left,
                       top: hobby.top,
                     }}
+                    drag
+                    dragConstraints={constraintRef}
                   >
                     <span className="text-gray-950 font-medium">
                       {hobby.title}
                     </span>
                     <span>{hobby.emoji}</span>
-                  </div>
+                  </motion.div>
                 ))}
               </div>
             </Card>
@@ -151,7 +163,9 @@ export const AboutSection = () => {
                 alt="Map"
                 className="h-full w-full object-cover object-left-top"
               />
-              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2  after:outline-offset-2 after:rounded-full after:outline-gray-950/30">
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full  after:content-[''] after:absolute after:inset-0 after:outline after:outline-2  after:outline-offset-2 after:rounded-full after:outline-gray-950/30">
+                <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-20 animate-ping [animation-duration:2s]"></div>
+                <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-10"></div>
                 <Image
                   src={smileEmoji}
                   alt="Smile Emoji"
