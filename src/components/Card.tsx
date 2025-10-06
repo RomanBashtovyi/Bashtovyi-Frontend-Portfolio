9e16f40d90d2a586be9462caa9b894bc2520bc40
Roman Bashtovyi <roman.bashtovyy@gmail.com>
2025-10-06 10:51:42 +0300

    feat(tailwind): add new animation 'move-left' and update Card component to accept additional props

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,12 @@
 import grainImage from '@/assets/images/grain.jpg'
 import { twMerge } from 'tailwind-merge'
-import { PropsWithChildren } from 'react'
+import { ComponentPropsWithRef } from 'react'
 
 export const Card = ({
   className,
   children,
-}: PropsWithChildren<{
-  className?: string
-}>) => {
+  ...other
+}: ComponentPropsWithRef<'div'>) => {
   return (
     <>
       <div
@@ -15,6 +14,7 @@ export const Card = ({
           "bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20  after:pointer-events-none",
           className
         )}
+        {...other}
       >
         <div
           className="absolute inset-0 -z-10 opacity-5"
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -57,54 +57,61 @@ export const ProjectsSection = () => {
           description="See how I transformed concepts into engaging digital experiences."
         />
         <div className="flex flex-col mt-10 gap-20 md:mt-20 ">
-          {portfolioProjects.map((project) => (
-            <Card
-              key={project.title}
-              className=" px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:px-20 lg:pt-16"
-            >
-              <div className="lg:grid lg:grid-cols-2 lg:gap-16">
-                <div className="lg:pb-16">
-                  <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text ">
-                    <span>{project.company}</span>
-                    <span>&bull;</span>
-                    <span>{project.year}</span>
+          {portfolioProjects.map(
+            (project, projectIndex) => (
+              <Card
+                key={project.title}
+                className=" px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:px-20 lg:pt-16 sticky"
+                style={{
+                  top: `calc(64px + ${
+                    projectIndex * 40
+                  }px)`,
+                }}
+              >
+                <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+                  <div className="lg:pb-16">
+                    <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text ">
+                      <span>{project.company}</span>
+                      <span>&bull;</span>
+                      <span>{project.year}</span>
+                    </div>
+                    <h3 className="text-2xl font-serif mt-2 md:text-4xl md:mt-5">
+                      {project.title}
+                    </h3>
+                    <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+                    <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+                      {project.results.map((result) => (
+                        <li
+                          key={result.title}
+                          className="flex gap-2 text-sm text-white/50 md:text-base "
+                        >
+                          <CheckCircleIcon className="size-5 md:size-6" />
+                          <span>{result.title}</span>
+                        </li>
+                      ))}
+                    </ul>
+                    <a
+                      href={project.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <button className="bg-white text-gray-950 w-full h-12 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8 md:w-auto px-6">
+                        <span>Visit Live Site</span>
+                        <ArrowUpRightIcon className="size-4" />
+                      </button>
+                    </a>
                   </div>
-                  <h3 className="text-2xl font-serif mt-2 md:text-4xl md:mt-5">
-                    {project.title}
-                  </h3>
-                  <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
-                  <ul className="flex flex-col gap-4 mt-4 md:mt-5">
-                    {project.results.map((result) => (
-                      <li
-                        key={result.title}
-                        className="flex gap-2 text-sm text-white/50 md:text-base "
-                      >
-                        <CheckCircleIcon className="size-5 md:size-6" />
-                        <span>{result.title}</span>
-                      </li>
-                    ))}
-                  </ul>
-                  <a
-                    href={project.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <button className="bg-white text-gray-950 w-full h-12 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8 md:w-auto px-6">
-                      <span>Visit Live Site</span>
-                      <ArrowUpRightIcon className="size-4" />
-                    </button>
-                  </a>
+                  <div className="relative">
+                    <Image
+                      src={project.image}
+                      alt={project.title}
+                      className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"
+                    />
+                  </div>
                 </div>
-                <div className="relative">
-                  <Image
-                    src={project.image}
-                    alt={project.title}
-                    className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"
-                  />
-                </div>
-              </div>
-            </Card>
-          ))}
+              </Card>
+            )
+          )}
         </div>
       </div>
     </section>
